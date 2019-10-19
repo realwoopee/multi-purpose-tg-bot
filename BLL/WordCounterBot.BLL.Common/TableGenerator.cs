170e968499333fac9c5cedc2844c59b3cc8e3222
Danila Pokhabov <danilapokhabov@protonmail.com>
2019-10-19 17:42:16 +0700

    Added GetCountersCommand

---
+++ BLL/WordCounterBot.BLL.Common/TableGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WordCounterBot.BLL.Common
+{
+    public static class TableGenerator
+    {
+        public static string GenerateTable(
+            string column1Name,
+            string column2Name,
+            IEnumerable<(object, object)> values,
+            int column1WidthLimit = 24,
+            int column2WidthLimit = 19)
+        {
+            var rows = values.Select(v => (v.Item1.ToString(), v.Item2.ToString()));
+
+            var col1Width = Math.Min(
+                Math.Max(rows.Max(v => v.Item1.Length), column1Name.Length), 
+                column1WidthLimit);
+
+            var col2Width = Math.Min(
+                Math.Max(rows.Max(v => v.Item2.Length),column2Name.Length), 
+                column2WidthLimit);
+
+            var sumWidth = col1Width + 1 + col2Width;
+
+            StringBuilder table = new StringBuilder();
+
+            var headers = GenerateRow(
+                column1Name, col1Width,
+                column2Name, col2Width);
+
+            table.AppendLine(headers);
+
+            table.AppendLine(new string('-', sumWidth));
+
+            foreach (var (item1, item2) in rows)
+            {
+                var rowStr = GenerateRow(
+                    Constrain(item1, col1Width), col1Width,
+                    Constrain(item2, col2Width), col2Width);
+                table.AppendLine(rowStr);
+            }
+
+            return table.ToString();
+        }
+
+        private static string GenerateRow(string col1, int col1Width, string col2, int col2Width) =>
+            col1 + new string(' ', col1Width - col1.Length) + "|" + new string(' ', col2Width - col2.Length) + col2;
+
+        private static string Constrain(string str, int limit, string tail = "...")
+        {
+            if (tail == null) throw new ArgumentNullException(nameof(tail));
+
+            if (str.Length <= limit)
+            {
+                return str;
+            }
+
+            return str.Substring(0, Math.Min(str.Length - 1 , limit - tail.Length - 1)) + tail;
+        }
+    }
+}
---
+++ BLL/WordCounterBot.BLL.Core/Commands/GetCountersCommand.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using WordCounterBot.BLL.Common;
+using WordCounterBot.DAL.Contracts;
+
+namespace WordCounterBot.BLL.Contracts 
+{
+    public class GetCountersCommand : ICommand
+    {
+        public string RegexPattern { get; } = @"get_top";
+
+        private IUserDao _userDao;
+        private TelegramBotClient _client;
+        private ICounterDao _counterDao;
+
+        public GetCountersCommand(ICounterDao counterDao, IUserDao userDao, TelegramBotClient client)
+        {
+            _userDao = userDao;
+            _client = client;
+            _counterDao = counterDao;
+        }
+
+        public async Task Execute(Update update, string command, params string[] args)
+        {
+            switch (command)
+            {
+                case "get_top_10":
+                    await GetTopNAndRespond(update, 10);
+                    break;
+                case "get_top_n":
+                    if (!int.TryParse(args[0], out var arg))
+                    {
+                        await  _client.SendTextMessageAsync(
+                            update.Message.Chat.Id,
+                            "Command argument is not a number",
+                            replyToMessageId: update.Message.MessageId);
+                    }
+                    await GetTopNAndRespond(update, arg);
+                    break;
+            }
+        }
+
+        private async Task GetTopNAndRespond(Update update, int N)
+        {
+            var counters = await _counterDao.GetCountersWithLimit(update.Message.Chat.Id, N);
+
+            var userId = update.Message.From.Id;
+
+            await _userDao.UpdateUser(update.Message.From);
+
+            var userCounters =
+                await Task.WhenAll(counters.Select(async (c) => new
+                {
+                    User = await _userDao.GetUserById(userId),
+                    Counter = c.counter
+                }));
+
+            var result = userCounters.Select(uc => 
+                ((object)(uc.User.FirstName + " " + uc.User.LastName), (object)uc.Counter)
+            ).ToList();
+
+            var table = TableGenerator.GenerateTable("user name", "words", result);
+
+            await _client.SendTextMessageAsync(
+                update.Message.Chat.Id,
+                table,
+                replyToMessageId: update.Message.MessageId);
+        }
+    }
+}
