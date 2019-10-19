219b278886ad8c4266f52753990ea5b4895e95b1
Danila Pokhabov <danilapokhabov@protonmail.com>
2019-10-19 17:36:33 +0700

    Added UserDao. Refactored CounterDao.

--- DAL/WordCounterBot.DAL.DatabaseAccess/CounterDao.cs
+++ DAL/WordCounterBot.DAL.DatabaseAccess/CounterDao.cs
@@ -19,94 +19,149 @@ namespace WordCounterBot.DAL.Postgresql
 
         public async Task AddCounter(long chatId, long userId)
         {
-            await _connection.OpenAsync();
-            await _connection.QueryAsync($@"insert into counters(""chatId"", ""userId"", counter)
+            try
+            {
+                await _connection.OpenAsync();
+                await _connection.QueryAsync($@"insert into counters(""chatId"", ""userId"", counter)
                                     values (@chatId, @userId, 0)",
-                                    new { chatId, userId });
-            await _connection.CloseAsync();
+                    new { chatId, userId });
+            }
+            finally
+            {
+                await _connection.CloseAsync();
+            }
         }
 
         public async Task AddCounter(long chatId, long userId, long counts)
         {
-            await _connection.OpenAsync();
-            await _connection.QueryAsync($@"insert into counters(""chatId"", ""userId"", counter)
+            try
+            {
+                await _connection.OpenAsync();
+                await _connection.QueryAsync($@"insert into counters(""chatId"", ""userId"", counter)
                                     values (@chatId, @userId, @counts)",
-                                    new { chatId, userId, counts });
-            await _connection.CloseAsync();
+                                        new { chatId, userId, counts });
+            }
+            finally
+            {
+                await _connection.CloseAsync();
+            }
         }
 
         public async Task IncrementCounter(long chatId, long userId)
         {
-            await _connection.OpenAsync();
-            await _connection.QueryAsync($@"update counters
+            try
+            {
+                await _connection.OpenAsync();
+                await _connection.QueryAsync($@"update counters
                                     set counter = counter + 1
                                  where counters.""chatId"" = @chatId and counters.""userId"" = @userId",
-                                new { chatId, userId });
-            await _connection.CloseAsync();
+                                    new { chatId, userId });
+            }
+            finally
+            {
+                await _connection.CloseAsync();
+            }
         }
 
         public async Task IncrementCounter(long chatId, long userId, long counts)
         {
-            await _connection.OpenAsync();
-            await _connection.QueryAsync($@"update counters
+            try
+            {
+                await _connection.OpenAsync();
+                await _connection.QueryAsync($@"update counters
                                     set counter = counter + @counts
                                  where counters.""chatId"" = @chatId and counters.""userId"" = @userId",
-                                    new { chatId, userId, counts });
-            await _connection.CloseAsync();
+                                        new { chatId, userId, counts });
+            }
+            finally
+            {
+                await _connection.CloseAsync();
+            }
         }
 
         public async Task ResetCounter(long chatId, long userId)
         {
-            await _connection.OpenAsync();
-            await _connection.QueryAsync($@"update counters
+            try
+            {
+                await _connection.OpenAsync();
+                await _connection.QueryAsync($@"update counters
                                     set counter = 0
                                  where counters.""chatId"" = @chatId and counters.""userId"" = @userId",
-                                        new { chatId, userId });
-            await _connection.CloseAsync();
+                                            new { chatId, userId });
+            }
+            finally
+            {
+                await _connection.CloseAsync();
+            }
         }
 
         public async Task<long> GetCounter(long chatId, long userId)
         {
-            await _connection.OpenAsync();
-            var value = await _connection.ExecuteScalarAsync<long>($@"select counter from counters
+            try
+            {
+                await _connection.OpenAsync();
+                var result = await _connection.QuerySingleAsync<long>($@"select counter from counters
                                  where counters.""chatId"" = @chatId and counters.""userId"" = @userId",
-                                            new { chatId, userId });
-            await _connection.CloseAsync();
-            return value;
+                    new { chatId, userId });
+                return result;
+            }
+            finally
+            {
+                await _connection.CloseAsync();
+            }
         }
 
         public async Task<List<(long userId, long counter)>> GetCounters(long chatId)
         {
-            await _connection.OpenAsync();
-            var value = await _connection.QueryAsync<(long, long)>($@"select ""userId"", counter from counters
+
+            try
+            {
+                await _connection.OpenAsync();
+                var result = await _connection.QueryAsync<(long, long)>($@"select ""userId"", counter from counters
                                      where counters.""chatId"" = @chatId
                                      order by counter desc",
-                new { chatId });
-            await _connection.CloseAsync();
-            return value.ToList();
+                    new { chatId });
+                return result.ToList();
+            }
+            finally
+            {
+                await _connection.CloseAsync();
+            }
         }
 
         public async Task<List<(long userId, long counter)>> GetCountersWithLimit(long chatId, int limit = 10)
         {
-            await _connection.OpenAsync();
-            var value = await _connection.QueryAsync<(long, long)>($@"select ""userId"", counter from counters
+            try
+            {
+                await _connection.OpenAsync();
+                var result = await _connection.QueryAsync<(long, long)>($@"select ""userId"", counter from counters
                                      where counters.""chatId"" = @chatId
                                      order by counter desc
                                      limit @limit",
-                new { chatId, limit });
-            await _connection.CloseAsync();
-            return value.ToList();
+                    new { chatId, limit });
+
+                return result.ToList();
+            }
+            finally
+            {
+                await _connection.CloseAsync();
+            }
         }
 
         public async Task<bool> CheckCounter(long chatId, long userId)
         {
-            await _connection.OpenAsync();
-            var value = await _connection.ExecuteScalarAsync<bool>($@"select exists(
+            try
+            {
+                await _connection.OpenAsync();
+                return await _connection.QuerySingleAsync<bool>($@"select exists(
                                      select 1 from counters
                                      where counters.""chatId"" = @chatId and counters.""userId"" = @userId)",
-                                                    new { chatId, userId });
-            await _connection.CloseAsync();
-            return value;
+                    new { chatId, userId });
+            }
+            finally
+            {
+                await _connection.CloseAsync();
+            }
         }
     }
 }
---
+++ DAL/WordCounterBot.DAL.DatabaseAccess/UserDao.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+using Dapper;
+using Npgsql;
+using Telegram.Bot.Types;
+using WordCounterBot.Common.Entities;
+using WordCounterBot.DAL.Contracts;
+
+namespace WordCounterBot.DAL.Postgresql
+{
+    public class UserDao : IUserDao
+    {
+        private readonly NpgsqlConnection _connection;
+        public UserDao(AppConfiguration appConfig)
+        {
+            _connection = new NpgsqlConnection(appConfig.DbConnectionString);
+        }
+
+        public async Task UpdateUser(User user)
+        {
+            try
+            {
+                await _connection.OpenAsync();
+                await _connection.QueryAsync(
+                    $@"insert into users(user_id, first_name, last_name, user_name)
+                       values (@user_id, @first_name, @last_name, @user_name)
+                       on conflict (user_id) do update
+                            set first_name = @first_name,
+                            last_name = @last_name,
+                            user_name = @user_name;",
+                    new
+                    {
+                        user_id = user.Id,
+                        first_name = user.FirstName,
+                        last_name = user.LastName,
+                        user_name = user.Username
+                    });
+            }
+            finally
+            {
+                await _connection.CloseAsync();
+            }
+        }
+
+        public async Task<User> GetUserById(long userId)
+        {
+            try
+            {
+                await _connection.OpenAsync();
+                var result = await _connection.QuerySingleAsync<User>(
+                    $@"select * from users where user_id = @user_id",
+                    new { user_id = userId });
+                return result;
+            }
+            finally
+            {
+                await _connection.CloseAsync();
+            }
+
+        }
+    }
+}
