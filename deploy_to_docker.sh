b423ef1b44f42d5060fdf8edb92acd5fe744a782
Danila Pokhabov <danilapokhabov@protonmail.com>
2019-11-03 05:21:46 +0700

    Fixed docker deploy script again

--- deploy_to_docker.sh
+++ deploy_to_docker.sh
@@ -1,5 +1,5 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build .
-docker tag  "multi-purpose-tg-bot" "$DOCKER_USERNAME/multi-purpose-tg-bot:$TRAVIS_TAG"
-docker tag  "multi-purpose-tg-bot" "$DOCKER_USERNAME/multi-purpose-tg-bot:latest"
+docker build -t "multi-purpose-tg-bot" .
+docker tag "multi-purpose-tg-bot" "$DOCKER_USERNAME/multi-purpose-tg-bot:$TRAVIS_TAG"
+docker tag "multi-purpose-tg-bot" "$DOCKER_USERNAME/multi-purpose-tg-bot:latest"
 docker push "$DOCKER_USERNAME/multi-purpose-tg-bot"
