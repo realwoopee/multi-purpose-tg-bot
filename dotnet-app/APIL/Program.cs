b38abebf97919821228dbbde8d8483c021290ba4
Danila Pokhabov <danilapokhabov@protonmail.com>
2019-11-03 02:42:51 +0700

    Fixed Dockerfile

--- dotnet-app/APIL/Program.cs
+++ dotnet-app/APIL/Program.cs
@@ -1,8 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using WordCounterBot.APIL.WebApi;
 
-namespace WordCounterBot
+namespace WordCounterBot.APIL.WebApi
 {
     public class Program
     {
