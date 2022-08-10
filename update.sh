cd17d7b6c01ecf5479fa6f6f76c99fe0e1f6c645
Danila Pokhabov <danilapokhabov@gmail.com>
2022-08-11 05:42:22 +0700

    Use nginx

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #replace ips from domains.json and ssl_env by the host ip
-sed -i "s/\([0-9]\{1,3\}.\)\{3\}[0-9]\{1,3\}/$(wget -qO - ifconfig.me/ip)/" domains.json
-sed -i "s/\([0-9]\{1,3\}.\)\{3\}[0-9]\{1,3\}/$(wget -qO - ifconfig.me/ip)/" ssl_env
+sed -i "s/\([0-9]\{1,3\}.\)\{3\}[0-9]\{1,3\}/$(wget -qO - ifconfig.me/ip)/" domains.json 
+sed -i "s/\([0-9]\{1,3\}.\)\{3\}[0-9]\{1,3\}/$(wget -qO - ifconfig.me/ip)/" substitute.env
+sed -i "s/\([0-9]\{1,3\}.\)\{3\}[0-9]\{1,3\}/$(wget -qO - ifconfig.me/ip)/" .env
 docker compose pull
 docker compose up -d --force-recreate
