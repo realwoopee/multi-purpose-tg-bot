3d84a5cbfacc6f7fdbb34cae2959f6723c9c0062
Danila Pokhabov <danilapokhabov@gmail.com>
2022-09-12 00:35:38 +0700

    Resolve circular dependency with cerbot & nginx

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #replace ips from domains.json and ssl_env by the host ip
-sed -i "s/\([0-9]\{1,3\}.\)\{3\}[0-9]\{1,3\}/$(wget -qO - ifconfig.me/ip)/" domains.json 
-sed -i "s/\([0-9]\{1,3\}.\)\{3\}[0-9]\{1,3\}/$(wget -qO - ifconfig.me/ip)/" substitute.env
-sed -i "s/\([0-9]\{1,3\}.\)\{3\}[0-9]\{1,3\}/$(wget -qO - ifconfig.me/ip)/" .env
+ip=$(wget -qO - ifconfig.me/ip)
+sed -i "s/\([0-9]\{1,3\}.\)\{3\}[0-9]\{1,3\}/$ip/" domains.json 
+sed -i "s/\([0-9]\{1,3\}.\)\{3\}[0-9]\{1,3\}/$ip/" substitute.env
+sed -i "s/\([0-9]\{1,3\}.\)\{3\}[0-9]\{1,3\}/$ip/" .env
 docker compose pull
 docker compose up -d --force-recreate
